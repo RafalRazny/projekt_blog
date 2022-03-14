b8bac13e108d945377c1a1d8603865a900c842c5
Rafał Raźny <rafal-razny@wp.pl>
2022-03-14 11:27:39 +0100

    Update glocg package

--- blog/routes.py
+++ blog/routes.py
@@ -1,8 +1,42 @@
-from flask import Flask, render_template
+from dataclasses import dataclass
+from flask import Flask, render_template, request
 from blog import app
+from blog.models import Entry, db
+from blog.forms import EntryForm
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def index():
-    return render_template("base.html")
+    all_posts = Entry.query.filter_by(is_published=True).order_by(Entry.pub_date.desc())
+    return render_template("homepage.html", all_posts=all_posts)
+
+@app.route("/new-post/", methods=["GET", "POST"])
+def create_entry():
+    form = EntryForm()
+    errors = None
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            entry = Entry(
+                title=form.title.data,
+                body=form.body.data,
+                is_published=form.is_published.data
+            )
+            db.session.add(entry)
+            db.session.commit()
+        else:
+            errors = form.errors
+    return render_template("entry_form.html", form=form, errors=errors)
+
+@app.route("/edit-post/<int:entry_id>", methods=["GET", "POST"])
+def edit_entry(entry_id):
+   entry = Entry.query.filter_by(id=entry_id).first_or_404()
+   form = EntryForm(obj=entry)
+   errors = None
+   if request.method == 'POST':
+       if form.validate_on_submit():
+           form.populate_obj(entry)
+           db.session.commit()
+       else:
+           errors = form.errors
+   return render_template("entry_form.html", form=form, errors=errors)
