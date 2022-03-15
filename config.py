6ab4e6249cc765383879c83c502616049e422b91
Rafał Raźny <rafal-razny@wp.pl>
2022-03-15 14:49:17 +0100

    Add values for log in and out

--- config.py
+++ config.py
@@ -1,11 +1,13 @@
 import os
 
-BASE_DIR = os.path.abspath(os.path.dirname(__file__))    # 1
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
-   SECRET_KEY = os.environ.get("SECRET_KEY") or "remember-to-add-secret-key"    # 2
-   SQLALCHEMY_DATABASE_URI = (                           # 3
+   SECRET_KEY = os.environ.get("SECRET_KEY") or "remember-to-add-secret-key"
+   SQLALCHEMY_DATABASE_URI = (
            os.environ.get('DATABASE_URL') or
            'sqlite:///' + os.path.join(BASE_DIR, 'newblog.db')
    )
    SQLALCHEMY_TRACK_MODIFICATIONS = False
+   ADMIN_USERNAME = os.environ.get("ADMIN_USERNAME", "admin")
+   ADMIN_PASSWORD = os.environ.get("ADMIN_PASSWORD", "change-me")
